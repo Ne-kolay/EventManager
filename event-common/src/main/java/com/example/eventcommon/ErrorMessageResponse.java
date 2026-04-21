c2c776a93ea00823c61d9a316792d09e6662cdb2
Ne_kolay <Yezzzhun@gmail.com>
2026-04-21 13:18:42 +0300

    minor fixes

---
+++ event-common/src/main/java/com/example/eventcommon/ErrorMessageResponse.java
@@ -0,0 +1,9 @@
+package com.example.eventcommon;
+
+import java.time.LocalDateTime;
+
+public record ErrorMessageResponse(
+        String message,
+        String detailedMessage,
+        LocalDateTime dateTime
+) {}
